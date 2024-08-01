a034621bac853e5caeab7e4d8dd2fc5d43793328
nnieun <abocado216@naver.com>
2024-08-01 09:02:12 +0900

    Feat: api test 추가

---
+++ KimNakyoung/aiAPI/README.md
@@ -0,0 +1,6 @@
+# dalle api test
+# 현재 TEST 상황
+### ![img.png](img.png)
+- 이미지 생성 확인 완료
+- 건당 약 60원
+- /ai-image 하면 생성해줌.
---
+++ KimNakyoung/aiAPI/src/main/java/com/sh/aiapi/ai/controller/AIController.java
@@ -0,0 +1,28 @@
+package com.sh.aiapi.ai.controller;
+
+import com.sh.aiapi.ai.service.AIService; // import 바꾸기 실제로는
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class AIController {
+    private final AIService aiService;
+// 사용할때 주석 푸세요
+
+//    @GetMapping("/ai-image")
+//    public String getImage(Model model)
+//            throws IOException, InterruptedException {
+//        String prompt = aiService.
+//                generatePictureV2("케이크 시안을 원하는데 동그란시트 겨울왕국 올라프케이크 생성해줘");
+//        model.addAttribute("url", prompt);
+//        return "test";
+//    }
+}
